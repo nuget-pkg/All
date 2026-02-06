3ca5908a4eb7fb00c20612430caf0431e7d3e592
JavaCommons Technologies <javacommons@gmail.com>
2026-02-07 02:23:49 +0900

    .

--- release.sh
+++ release.sh
@@ -4,7 +4,4 @@ set -e
 rm -rf bin obj packages
 dotnet restore MyApi.sln -p:Configuration=Release -p:Platform="Any CPU"
 msbuild.exe MyApi.sln -p:Configuration=Release -p:Platform="Any CPU"
-#dotnet build -c Release
-#cp -rv bin/Release/net9.0/win-x64/* ~/cmd/
-#cp -rv bin/Release/net481/win-x64/* ~/cmd/dotnet4/
-cp -rv bin/Release/net481/* ~/cmd/
+cp -rv bin/Release/net462/* ~/cmd/
